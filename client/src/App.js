73827b493931feabc3afab753ea32a7b2cbf0223
Shubham-503 <kr.shubham025@gmail.com>
2022-11-21 20:29:00 +0530

    Added auto todo fetch on creation

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,41 @@ import './App.css';
 import axios from 'axios'
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState("")
 
   const createTodo = async (title) => {
     try {
       const res = await axios.post('/createtodo', { title })
+      console.log(res)
+    getTodos()
+
     } catch (error) {
       console.log(error.message);
     }
   }
 
+  const getTodos = async () => {
+    try {
+      const res = await axios.get('/gettodos')
+       setTodos(res.data.todos)
+      console.log(todos);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  useEffect(() => {
+    getTodos()
+  },[])
+  
+
 
   return (
     <div className="App">
       <TodoForm createTodo={createTodo} />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 }
--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+  console.log(todos)
   return (
     <div className='todoList'>
+      {todos && todos.map(todo => {
+        console.log(">>>>", todo)
+        return <div className="">
+          <h2>{todo.title}</h2>
+          {todo.tasks && todo.tasks.map((task) => {
+            return <p>{task}</p>
+          })}
+          <div className="tasks">
+
+          </div>
+        </div>
+      })}
       <h2>todo</h2>
       <div className="tasks">
         <p>task</p>
